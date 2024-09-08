260f565e4852d5f5a33a1ae9f5ebd06a5c1467ed
swayam <113868039+swyxm@users.noreply.github.com>
2024-09-07 20:09:24 -0700

    Update emailBot.py

--- emailBot.py
+++ emailBot.py
@@ -1,13 +1,4 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sun May 16 21:00:51 2020
-
-@author: swayam
-"""
-#Simple email bot I developed (Can send up to 500 emails within 1 minute before being timed out)
-#Sadly won't work anymore and it's out of my control, Google removed the third-party access from modules feature for account security
-#To help keep your account secure, from May 30, 2022, ​​Google no longer supports the use of third-party apps or devices which ask you to sign in to your Google Account using only your username and password.
+#Created on Sun May 16 2020
 
 import smtplib
 from getpass import getpass
